23721fd01595d55b7637c986d0d563e3d2872ad8
Anshu Dubey <anshudubey7415@gmail.com>
2026-07-21 15:44:18 +0530

    Updated backend API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sudisha-foundation-management.onrender.com",
 });
 
 // Interceptor add kar diya hai
